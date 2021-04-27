313d1e0bb88d9ef2db458a65ba4c9c2583564ee0
akassh <akassh.shah77@gmail.com>
2021-04-26 20:42:16 -0400

    removing java setup stuff & no longer re-downloading data

--- setup.sh
+++ setup.sh
@@ -6,17 +6,25 @@
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install -y default-jre default-jdk unzip python3-pip
 
+
+
+# no longer needed, i think
+
 # get the aws sdk for java
 
-wget https://sdk-for-java.amazonwebservices.com/latest/aws-java-sdk.zip
+# wget https://sdk-for-java.amazonwebservices.com/latest/aws-java-sdk.zip
 
 # unzip and change the directory name to be more manageable
 
-x=$(unzip -qql aws-java-sdk.zip | head -n1 | tr -s ' ' | cut -d' ' -f5-)
+# x=$(unzip -qql aws-java-sdk.zip | head -n1 | tr -s ' ' | cut -d' ' -f5-)
 
-unzip aws-java-sdk.zip
+# unzip aws-java-sdk.zip
 
-mv $x aws_sdk
+# mv $x aws_sdk
+
+
+
+
 
 # get spark + hadoop
 
@@ -32,18 +40,33 @@ mv $y spark_bin_hadoop
 
 rm -rf aws-java-sdk.zip spark-3.1.1-bin-hadoop3.2.tgz
 
+
+
+
+# no longer needed i think
+
 # set CLASSPATH variable & PATH variable
 
-export CLASSPATH=$(pwd)/aws_sdk/lib/*:$(pwd)/aws_sdk/third-party/lib/*:$(pwd)/spark_bin_hadoop/jars/*:
-export PATH=$PATH:$(pwd)/spark_bin_hadoop/bin
+# export CLASSPATH=$(pwd)/aws_sdk/lib/*:$(pwd)/aws_sdk/third-party/lib/*:$(pwd)/spark_bin_hadoop/jars/*:
+# export PATH=$PATH:$(pwd)/spark_bin_hadoop/bin
+
+
+
+
+
+# not needed to re-download files
 
 # remove datasets if exist and re-get
 
-rm TrainingDataset-fixed-libsvm.txt ValidationDataset-fixed-libsvm.txt
+# rm TrainingDataset-fixed-libsvm.txt ValidationDataset-fixed-libsvm.txt
 
-wget http://web.njit.edu/~as2757/cs643/TrainingDataset-fixed-libsvm.txt
+# wget http://web.njit.edu/~as2757/cs643/TrainingDataset-fixed-libsvm.txt
 
-wget http://web.njit.edu/~as2757/cs643/ValidationDataset-fixed-libsvm.txt
+# wget http://web.njit.edu/~as2757/cs643/ValidationDataset-fixed-libsvm.txt
+
+
+
+
 
 pip3 install pyspark numpy
 
