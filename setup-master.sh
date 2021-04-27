d212aafffe980c900c776dbeb5c21cb10609c512
akassh <akassh.shah77@gmail.com>
2021-04-26 23:15:20 -0400

    .

--- setup-master.sh
+++ setup-master.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+eval `ssh-agent`
+
+ssh-add ssh_keys/node_comms
+
 cat ip_of_workers > spark_bin_hadoop/conf/workers
 
 # replace with the IP address of your master
 
-echo "spark.master                     spark://172.31.61.29:7077" > spark_bin_hadoop/conf/spark-defaults.conf
+echo "spark.master                     spark://172.31.84.38:7077" > spark_bin_hadoop/conf/spark-defaults.conf
 
